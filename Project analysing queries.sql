409363efdf7d2d4260e05a36341628d9c555474f
mrkulkarni108 <1012411215@despu.edu.in>
2026-04-29 01:36:29 +0530

    Add files via upload

---
+++ Project analysing queries.sql
@@ -0,0 +1,17 @@
+show databases;
+use food_delivery;
+show tables;
+
+desc food_items;
+desc order_items;
+desc orders;
+desc payments;
+desc restaurants;
+desc users;
+
+select * from food_items;
+select * from order_items;
+select * from orders;
+select * from payments;
+select * from restaurants;
+select * from users;
